693c598ae3dea0fd56358fd08c925be6c585a8b1
Matthijs Kolkman <mkolkman@f0r4s19.codam.nl>
2019-08-29 21:02:25 +0200

    add

--- practice/ft_atoi.c
+++ practice/ft_atoi.c
@@ -1,23 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_functions.h                                     :+:    :+:            */
+/*   ft_atoi.c                                          :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: mkolkman <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/08/27 16:30:34 by mkolkman      #+#    #+#                 */
-/*   Updated: 2019/08/27 16:30:39 by mkolkman      ########   odam.nl         */
+/*   Created: 2019/08/29 17:36:12 by mkolkman      #+#    #+#                 */
+/*   Updated: 2019/08/29 17:36:13 by mkolkman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_FUNCTIONS_H
-# define FT_FUNCTIONS_H
+#include "ft_functions.h"
 
-#include <unistd.h>
-#include <stdio.h>
+int		ft_atoi(const char *str)
+{
+	int 	new;
 
-void	ft_putstr(char *str);
-char    *ft_strcpy(char *s1, char *s2);
-void	ft_putnbr(int nbr);
+	new = 0;
+	if()
+	while (*str)
+	{
+		new = (new * 10) + *str - '0';
+		str++;
+	}
+	// TO DO add -
+	return(new);
+}
 
-#endif
+
+int 	main(void)
+{
+	ft_putnbr(ft_atoi("2147483647"));
+
+	return 0;
+}
--- practice/ft_putnbr.c
+++ practice/ft_putnbr.c
@@ -1,23 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_functions.h                                     :+:    :+:            */
+/*   ft_putnbr.c                                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: mkolkman <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/08/27 16:30:34 by mkolkman      #+#    #+#                 */
-/*   Updated: 2019/08/27 16:30:39 by mkolkman      ########   odam.nl         */
+/*   Created: 2019/08/29 17:42:05 by mkolkman      #+#    #+#                 */
+/*   Updated: 2019/08/29 17:42:08 by mkolkman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_FUNCTIONS_H
-# define FT_FUNCTIONS_H
+#include "ft_functions.h"
 
-#include <unistd.h>
-#include <stdio.h>
+void	ft_putnbr(int nbr)
+{
+	char 	c;
 
-void	ft_putstr(char *str);
-char    *ft_strcpy(char *s1, char *s2);
-void	ft_putnbr(int nbr);
-
-#endif
+	// 32556
+	if(nbr == -2147483648)
+	{
+		ft_putnbr(-214748364);
+		write(1, "8", 1);
+		return ;
+	}
+	if(nbr < 0)
+	{
+		write(1, "-", 1);
+		nbr = nbr * -1;
+	}
+	if(nbr >= 10)
+		ft_putnbr(nbr / 10);
+	c = (nbr % 10) + '0';
+	write(1, &c, 1);
+}
